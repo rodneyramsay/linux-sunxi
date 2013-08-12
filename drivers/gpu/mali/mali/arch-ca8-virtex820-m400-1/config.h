92a7277e55b7c6af112c9edb3743b0244a1e02d3
benn <benn@cubietech.com>
2013-08-12 20:18:39 +0800

    mali: we can load mali successfully now

--- drivers/gpu/mali/mali/arch-ca8-virtex820-m400-1/config.h
+++ drivers/gpu/mali/mali/arch-ca8-virtex820-m400-1/config.h
@@ -13,63 +13,83 @@
 
 /* Configuration for the EB platform with ZBT memory enabled */
 
+#define SW_FB_MEM_BASE (0x40000000 - SZ_512M - SZ_64M - SZ_32M)
+#define SW_FB_MEM_SIZE SZ_32M
+#define SW_GPU_MEM_BASE (0x40000000 - SZ_512M - SZ_64M)
+#define SW_GPU_MEM_SIZE SZ_64M
+
+
 static _mali_osk_resource_t arch_configuration [] =
 {
 	{
 		.type = PMU,
 		.description = "Mali-400 PMU",
 		.base = 0x01C42000,
-		.irq = 73,
+		.irq = SW_INT_IRQNO_GPU_PMU,
 		.mmu_id = 0
 	},
 	{
 		.type = MALI400GP,
 		.description = "Mali-400 GP",
 		.base = 0x01C40000,
-		.irq = 69,
+		.irq = SW_INT_IRQNO_GPU_GP,
 		.mmu_id = 1
 	},
 	{
 		.type = MALI400PP,
 		.base = 0x01C48000,
-		.irq = 71,
-		.description = "Mali-400 PP",
+		.irq = SW_INT_IRQNO_GPU_PP0,
+		.description = "Mali-400 PP0",
 		.mmu_id = 2
+	},
+	{
+		.type = MALI400PP,
+		.base = 0x01C4A000,
+		.irq = SW_INT_IRQNO_GPU_PP1,
+		.description = "Mali-400 PP1",
+		.mmu_id = 3
 	},
 	{
 		.type = MMU,
 		.base = 0x01C43000,
-		.irq = 70,
+		.irq = SW_INT_IRQNO_GPU_GPMMU,
 		.description = "Mali-400 MMU for GP",
 		.mmu_id = 1
 	},
 	{
 		.type = MMU,
 		.base = 0x01C44000,
-		.irq = 72,
-		.description = "Mali-400 MMU for PP",
+		.irq = SW_INT_IRQNO_GPU_PPMMU0,
+		.description = "Mali-400 MMU for PP0",
 		.mmu_id = 2
+	},
+	{
+		.type = MMU,
+		.base = 0x01C45000,
+		.irq = SW_INT_IRQNO_GPU_PPMMU1,
+		.description = "Mali-400 MMU for PP1",
+		.mmu_id = 3
 	},
 	{
 		.type = MEMORY,
 		.description = "Mali Sdram",
 		.alloc_order = 0,    /* highest preference for this memory */
-		.base = 0x5C000000,
-		.size = 64 * 1024 * 1024, /*64M*/
+		.base = SW_GPU_MEM_BASE,
+		.size = SW_GPU_MEM_SIZE, /*64M*/
 		.flags = _MALI_CPU_WRITEABLE | _MALI_CPU_READABLE | _MALI_MMU_READABLE | _MALI_MMU_WRITEABLE
 	},
 	{
 		.type = MEM_VALIDATION,
 		.description = "Framebuffer",
-		.base = 0x5A000000,
-		.size = 32 * 1024 * 1024,  /*32M*/
+		.base = SW_FB_MEM_BASE,
+		.size = SW_FB_MEM_SIZE,  /*32M*/
 		.flags = _MALI_CPU_WRITEABLE | _MALI_CPU_READABLE | _MALI_MMU_READABLE | _MALI_MMU_WRITEABLE
 	},
 	{
 		.type = OS_MEMORY,
 		.description = "OS Memory",
 		.alloc_order = 1, /* Lowest preference for this memory */
-		.size = 192 * 1024 * 1024, /* 64 MB */
+		.size = 512 * 1024 * 1024, /* 64 MB */
 		.flags = _MALI_CPU_WRITEABLE | _MALI_CPU_READABLE | _MALI_MMU_READABLE | _MALI_MMU_WRITEABLE
 	},
 	{
